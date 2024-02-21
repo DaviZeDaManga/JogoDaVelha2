ec149edb78d2d6299930034835238b81313c70b3
Davi Matinho <129508589+DaviZeDaManga@users.noreply.github.com>
2024-02-21 12:42:00 -0300

    criar sistema para logar, melhorar dessign e fazer modo empate

--- site/src/game.js
+++ site/src/game.js
@@ -10,6 +10,7 @@ function GameApp() {
 
 
 
+  
 
   //jogo principal
   const [jogandoojogo, setJogandoojogo] = useState(null)
@@ -18,34 +19,6 @@ function GameApp() {
     "3","4","5",
     "6","7","8",
   ]);
-
-  //verificar quem ganhou o jogo principal
-  useEffect(() => {
-    const linhas = [
-      [0, 1, 2], // Linha superior
-      [3, 4, 5], // Linha do meio
-      [6, 7, 8], // Linha inferior
-      [0, 3, 6], // Coluna esquerda
-      [1, 4, 7], // Coluna do meio
-      [2, 5, 8], // Coluna direita
-      [0, 4, 8], // Diagonal principal
-      [2, 4, 6], // Diagonal secundária
-    ];
-
-    for (let linha of linhas) {
-      const [a, b, c] = linha;
-      if ( jogoprincipal[a] == jogoprincipal[b] && jogoprincipal[b] == jogoprincipal[c] && jogoprincipal[c] == jogoprincipal[a]) {
-        window.location.reload()
-      }
-    }
-  }, [jogoprincipal]);
-
-
-
-
-
-
-
   //os jogso em array
   const [jogos, setJogos] = useState([
     ["0", "1", "2", 
@@ -84,6 +57,14 @@ function GameApp() {
     "3", "4", "5", 
     "6", "7", "8"],
   ]);
+
+  
+
+
+
+
+
+
 
   //adicionar marcacao no jogo especifico
   function AdicionarMarcacao(jogo, posicao) {
@@ -129,6 +110,17 @@ function GameApp() {
 
     
   }
+  console.log(jogoprincipal)
+
+  //verificar se algum jogo esta na casa errada
+  useEffect(()=> {
+    for (let item of jogoprincipal) {
+      const position = jogoprincipal.indexOf(item)
+      if ((item == "x" || item == "o") && position == jogandoojogo) {
+        setJogandoojogo(null)
+      }
+    }
+  })
 
   //verificar se algum jogo foi ganho
   useEffect(() => {
@@ -144,12 +136,12 @@ function GameApp() {
     ];
 
     for (let jogo of jogos) {
+      let todasAsCasasPreenchidas = true;
+  
       for (let linha of linhas) {
         const [a, b, c] = linha;
   
-        if (
-          jogo[a] === jogo[b] && jogo[b] === jogo[c] && jogo[c] === jogo[a] 
-        ) {
+        if ( jogo[a] === jogo[b] && jogo[b] === jogo[c] && jogo[c] === jogo[a] ) {
           let copiaprincipal = [...jogoprincipal];
           const index = jogos.indexOf(jogo);
   
@@ -157,23 +149,49 @@ function GameApp() {
             copiaprincipal[index] = vezdojogador === 1 ? "o" : "x";
             setJogoprincipal(copiaprincipal);
           }
+  
+          else if (todasAsCasasPreenchidas) {
+            copiaprincipal[index] = "empate"
+            setJogoprincipal(copiaprincipal);
+          }
         }
       }
     }
   }, [jogos]);
 
+  //verificar quem ganhou o jogo principal
+  useEffect(() => {
+    const linhas = [
+      [0, 1, 2], // Linha superior
+      [3, 4, 5], // Linha do meio
+      [6, 7, 8], // Linha inferior
+      [0, 3, 6], // Coluna esquerda
+      [1, 4, 7], // Coluna do meio
+      [2, 5, 8], // Coluna direita
+      [0, 4, 8], // Diagonal principal
+      [2, 4, 6], // Diagonal secundária
+    ];
+
+    for (let linha of linhas) {
+      const [a, b, c] = linha;
+      if ( jogoprincipal[a] == jogoprincipal[b] && jogoprincipal[b] == jogoprincipal[c] && jogoprincipal[c] == jogoprincipal[a]) {
+        window.location.reload()
+      }
+    }
+  }, [jogoprincipal]);
 
 
 
 
-  console.log(jogoprincipal)
+
+
 
   return (
     <div className="JogoDaVelha2">
 
       <section className="jogo">
         {jogoprincipal.map((item, index) => (
-          <div key={index} className={`cards-jogo_principal ${item == "x" && "x"} ${ item == "o" && "o"} ${jogandoojogo == index && "jogando"}`}>
+          <div key={index} className={`cards-jogo_principal ${item == "x" && "x"} ${ item == "o" && "o"} ${item == "empate" && "empate"} ${jogandoojogo == index && "jogando"}`}>
 
             {item != "x" || item != "o"} {
               <>
@@ -188,7 +206,7 @@ function GameApp() {
                 <div onClick={() => AdicionarMarcacao(index, indexInterno)} key={indexInterno} className={`cards-jogos ${jogandoojogo === index && (itemInterno !== "x" && itemInterno !== "o") && "jogando"} o`}> 
                   <img src="/assets/images/roda.png" />
                 </div>}
-                {itemInterno !== "x" && itemInterno !== "o" &&
+                {itemInterno !== "x" && itemInterno !== "o" && itemInterno !== "empate" &&
                 <div onClick={() => AdicionarMarcacao(index, indexInterno)} key={indexInterno} className={`cards-jogos ${jogandoojogo === index && (itemInterno !== "x" && itemInterno !== "o") && "jogando"}`}> </div>}
                 
                 </>
@@ -200,6 +218,9 @@ function GameApp() {
             }
             {item == "o" &&
             <div className="oganhou"><img src="/assets/images/roda.png" /></div>
+            }
+            {item == "empate" &&
+            <div className="empate"></div>
             }
 
           </div>
