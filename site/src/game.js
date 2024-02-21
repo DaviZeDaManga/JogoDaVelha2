a1f3436cd960cf3ec2d923f831c6dcbba419c7ba
Davi Matinho <129508589+DaviZeDaManga@users.noreply.github.com>
2024-02-21 01:41:04 -0300

    ta ficando foda, a lógica ta pronta, falta design e login

--- site/src/game.js
+++ site/src/game.js
@@ -1,149 +1,211 @@
 import "./game.scss";
 import { useEffect, useState } from "react";
 
+//bibliotecas
+import { ToastContainer, toast } from "react-toastify";
+
 function GameApp() {
   const [vitoria, setVitoria] = useState("jogando");
   const [vezdojogador, setVezdojogador] = useState(1);
 
+
+
+
+  //jogo principal
+  const [jogandoojogo, setJogandoojogo] = useState(null)
   const [jogoprincipal, setJogoprincipal] = useState([
     "0","1","2",
     "3","4","5",
     "6","7","8",
   ]);
 
-  //verificar quem ganhou o jogo ao todo
-  useEffect(()=> {
+  //verificar quem ganhou o jogo principal
+  useEffect(() => {
     const linhas = [
-        [0, 1, 2], // Linha superior
-        [3, 4, 5], // Linha do meio
-        [6, 7, 8], // Linha inferior
-        [0, 3, 6], // Coluna esquerda
-        [1, 4, 7], // Coluna do meio
-        [2, 5, 8], // Coluna direita
-        [0, 4, 8], // Diagonal principal
-        [2, 4, 6], // Diagonal secundária
-      ];
-    
+      [0, 1, 2], // Linha superior
+      [3, 4, 5], // Linha do meio
+      [6, 7, 8], // Linha inferior
+      [0, 3, 6], // Coluna esquerda
+      [1, 4, 7], // Coluna do meio
+      [2, 5, 8], // Coluna direita
+      [0, 4, 8], // Diagonal principal
+      [2, 4, 6], // Diagonal secundária
+    ];
+
     for (let linha of linhas) {
-        const [a, b, c] = linha;
-        if (jogoprincipal[a] == jogoprincipal[b] && jogoprincipal[b] == jogoprincipal[c] && jogoprincipal[c] == jogoprincipal[a]) {
-            setVitoria(`Vitória do jogador ${vezdojogador}`);
-            return; // Se encontrou uma vitória, não é necessário verificar mais linhas
-        }
+      const [a, b, c] = linha;
+      if ( jogoprincipal[a] == jogoprincipal[b] && jogoprincipal[b] == jogoprincipal[c] && jogoprincipal[c] == jogoprincipal[a]) {
+        window.location.reload()
+      }
     }
-  }, [jogoprincipal])
+  }, [jogoprincipal]);
+
+
+
 
 
 
 
   //os jogso em array
   const [jogos, setJogos] = useState([
-    ["0","1","2",
-    "3","4","5",
-    "6","7","8",],
+    ["0", "1", "2", 
+    "3", "4", "5", 
+    "6", "7", "8"],
 
-    ["0","1","2",
-    "3","4","5",
-    "6","7","8",],
+    ["0", "1", "2", 
+    "3", "4", "5", 
+    "6", "7", "8"],
 
-    ["0","1","2",
-    "3","4","5",
-    "6","7","8",],
+    ["0", "1", "2", 
+    "3", "4", "5", 
+    "6", "7", "8"],
 
-    ["0","1","2",
-    "3","4","5",
-    "6","7","8",],
+    ["0", "1", "2", 
+    "3", "4", "5", 
+    "6", "7", "8"],
 
-    ["0","1","2",
-    "3","4","5",
-    "6","7","8",],
+    ["0", "1", "2", 
+    "3", "4", "5", 
+    "6", "7", "8"],
 
-    ["0","1","2",
-    "3","4","5",
-    "6","7","8",],
+    ["0", "1", "2", 
+    "3", "4", "5", 
+    "6", "7", "8"],
 
-    ["0","1","2",
-    "3","4","5",
-    "6","7","8",],
+    ["0", "1", "2", 
+    "3", "4", "5", 
+    "6", "7", "8"],
 
-    ["0","1","2",
-    "3","4","5",
-    "6","7","8",],
+    ["0", "1", "2", 
+    "3", "4", "5", 
+    "6", "7", "8"],
 
-    ["0","1","2",
-    "3","4","5",
-    "6","7","8",],
+    ["0", "1", "2", 
+    "3", "4", "5", 
+    "6", "7", "8"],
   ]);
 
   //adicionar marcacao no jogo especifico
   function AdicionarMarcacao(jogo, posicao) {
-    const copiaJogos = [...jogos]; // Criando uma cópia do array original
-    const jogoespecifico = copiaJogos[jogo] // pegando o jogo especifico 
+    const copiaJogos = [...jogos]; // Criando uma cópia dos jogos
+    const jogoespecifico = copiaJogos[jogo]; // pegando o jogo especifico
 
-    if (vezdojogador === 1 || vezdojogador === 2) { 
-        jogoespecifico[posicao] = vezdojogador === 1 ? "x" : "o";
-        copiaJogos[jogo] = jogoespecifico
+    try {
 
-        setJogos(copiaJogos); // Atualiza o estado do array
+      if (jogoespecifico[posicao] == "x" || jogoespecifico[posicao] == "o") {
+        alert("essa casa ja foi jogada")
+      }
 
-        if (vezdojogador == 1) { setVezdojogador(2) }
-        else if (vezdojogador == 2) { setVezdojogador(1) }
-    }
-  }
+      else if (jogandoojogo != jogo && jogandoojogo != null ) {
+        alert("essa casa nao pode ser jogada")
+      }
+      
+      else if (jogandoojogo == jogo || jogandoojogo == null) {
 
-  //verificar se algum jogo foi ganho
-  useEffect(()=> {
-    const linhas = [
-        [0, 1, 2], // Linha superior
-        [3, 4, 5], // Linha do meio
-        [6, 7, 8], // Linha inferior
-        [0, 3, 6], // Coluna esquerda
-        [1, 4, 7], // Coluna do meio
-        [2, 5, 8], // Coluna direita
-        [0, 4, 8], // Diagonal principal
-        [2, 4, 6], // Diagonal secundária
-    ]
-
-    //errado
-    // for (let itemjogos of jogos) {
-    //     let jogo = jogos[itemjogos]
-
-    //     for (let linha of linhas) {
-    //         let [a, b, c] = linha
-
-    //         if (jogo[a] == jogo[b] && jogo[b] == jogo[c] && jogo[c] == jogo[a]) {
-    //             alert("foi porra")
-    //         }
-    //     }
-    // }
-
-    for (let jogo of jogos) {
-        for (let linha of linhas) {
-          const [a, b, c] = linha;
-    
-          if (jogo[a] && jogo[a] === jogo[b] && jogo[a] === jogo[c]) {
-            alert("Vitória!");
-            return; // Encerra a função se encontrarmos uma vitória
+        if (vezdojogador === 1 || vezdojogador === 2) {
+          jogoespecifico[posicao] = vezdojogador === 1 ? "x" : "o";
+          copiaJogos[jogo] = jogoespecifico;
+  
+          setJogos(copiaJogos); // Atualiza o estado do array
+          
+          if (jogoprincipal[posicao] == "x" || jogoprincipal[posicao] == "o" ) {
+            setJogandoojogo(null)
+          }
+          else {
+            setJogandoojogo(posicao)
           }
         }
       }
-  }, [jogos])
+    }
+    catch {
+
+      toast.error("Parece que algo deu errado")
+    }
+    finally {
+
+      if (vezdojogador == 1) { setVezdojogador(2); } 
+      else if (vezdojogador == 2) { setVezdojogador(1); }
+    }
+
+    
+  }
+
+  //verificar se algum jogo foi ganho
+  useEffect(() => {
+    const linhas = [
+      [0, 1, 2], // Linha superior
+      [3, 4, 5], // Linha do meio
+      [6, 7, 8], // Linha inferior
+      [0, 3, 6], // Coluna esquerda
+      [1, 4, 7], // Coluna do meio
+      [2, 5, 8], // Coluna direita
+      [0, 4, 8], // Diagonal principal
+      [2, 4, 6], // Diagonal secundária
+    ];
+
+    for (let jogo of jogos) {
+      for (let linha of linhas) {
+        const [a, b, c] = linha;
+  
+        if (
+          jogo[a] === jogo[b] && jogo[b] === jogo[c] && jogo[c] === jogo[a] 
+        ) {
+          let copiaprincipal = [...jogoprincipal];
+          const index = jogos.indexOf(jogo);
+  
+          if (copiaprincipal[index] !== "x" && copiaprincipal[index] !== "o") {
+            copiaprincipal[index] = vezdojogador === 1 ? "o" : "x";
+            setJogoprincipal(copiaprincipal);
+          }
+        }
+      }
+    }
+  }, [jogos]);
+
+
+
+
+
+  console.log(jogoprincipal)
 
   return (
     <div className="JogoDaVelha2">
+
       <section className="jogo">
+        {jogoprincipal.map((item, index) => (
+          <div key={index} className={`cards-jogo_principal ${item == "x" && "x"} ${ item == "o" && "o"} ${jogandoojogo == index && "jogando"}`}>
 
-            {jogoprincipal.map((item, index) => (
-            <div key={index} className={`cards-jogo_principal ${item === "x" && "x"} ${item === "o" && "o"}`}>
+            {item != "x" || item != "o"} {
+              <>
+              {jogos[index].map((itemInterno, indexInterno) => (
+                <>
+
+                {itemInterno === "x" &&
+                <div onClick={() => AdicionarMarcacao(index, indexInterno)} key={indexInterno} className={`cards-jogos ${jogandoojogo === index && (itemInterno !== "x" && itemInterno !== "o") && "jogando"} x`}> 
+                  <img src="/assets/images/x.png" />
+                </div>}
+                {itemInterno === "o" &&
+                <div onClick={() => AdicionarMarcacao(index, indexInterno)} key={indexInterno} className={`cards-jogos ${jogandoojogo === index && (itemInterno !== "x" && itemInterno !== "o") && "jogando"} o`}> 
+                  <img src="/assets/images/roda.png" />
+                </div>}
+                {itemInterno !== "x" && itemInterno !== "o" &&
+                <div onClick={() => AdicionarMarcacao(index, indexInterno)} key={indexInterno} className={`cards-jogos ${jogandoojogo === index && (itemInterno !== "x" && itemInterno !== "o") && "jogando"}`}> </div>}
                 
-                {jogos[index].map((itemInterno, indexInterno) => (
-                    <div onClick={() => AdicionarMarcacao(index, indexInterno)} key={indexInterno} className={`cards-jogos ${itemInterno == "x" && 'x'} ${itemInterno == "o" && 'o'}`}></div>
-                ))}
+                </>
+              ))}
+              </>
+            }
+            {item == "x" &&
+            <div className="xganhou"><img src="/assets/images/x.png" /></div>
+            }
+            {item == "o" &&
+            <div className="oganhou"><img src="/assets/images/roda.png" /></div>
+            }
 
-            </div>
-            ))}
-
+          </div>
+        ))}
       </section>
+
     </div>
   );
 }
